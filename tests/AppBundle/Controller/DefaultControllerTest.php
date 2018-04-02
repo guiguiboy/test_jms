63f7d04f41a840a98f627c5f15a98b30d5fb76b2
Guiguiboy <guillaume.bretou@gmail.com>
2018-04-02 09:47:39 +0200

    Playing with denormalizer

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\AppBundle\Controller;
 
+use AppBundle\Model\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
@@ -14,5 +15,25 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+    public function testDenormalizer()
+    {
+        static::createClient();
+        $serializer = static::$kernel->getContainer()->get('serializer');
+
+        $userToPopulate = new User();
+        $userToPopulate->setLogin('this will be replaced');
+
+        $input = [
+            'login' => 'guiguiboy',
+            'firstname' => 'guigui',
+            'lastname' => 'boy',
+        ];
+
+        $serializer->denormalize($input, User::class, null, [
+            'object_to_populate' => $userToPopulate
+        ]);
+        $this->assertEquals('guiguiboy', $userToPopulate->getLogin());
     }
 }
